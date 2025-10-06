f1ab75e37ef70ed41996f4a6f8c242169ee76670
leslie helou <leslie@ccs-indy.com>
2025-10-06 14:17:52 -0400

    cleanup

--- Replicator/Queues.swift
+++ Replicator/Queues.swift
@@ -8,58 +8,6 @@
 
 import Foundation
 
-/*
-final class SendQueue {
-    static let shared = SendQueue()
-    
-    let operationQueue: OperationQueue
-    private let monitorQueue = DispatchQueue(label: "replicator.sendQueue.queueMonitor", qos: .background)
-    private var isMonitoring = false
-    private let monitorLock = NSLock()
-
-    /// Called when the queue becomes empty.
-    var onQueueEmpty: (() -> Void)?
-
-    private init() {
-        let queue = OperationQueue()
-        queue.name = "jamf.replicator.sendQueue"
-        queue.maxConcurrentOperationCount = maxConcurrentThreads
-        queue.qualityOfService = .userInitiated
-        self.operationQueue = queue
-    }
-
-    func addOperation(_ block: @escaping () -> Void) {
-        operationQueue.addOperation(block)
-        startMonitoring()
-    }
-
-    private func startMonitoring() {
-        monitorLock.lock()
-        defer { monitorLock.unlock() }
-
-        guard !isMonitoring else { return }
-        isMonitoring = true
-
-        monitorQueue.async { [weak self] in
-            guard let self = self else { return }
-
-            while true {
-                // Sleep briefly to avoid busy-waiting
-                Thread.sleep(forTimeInterval: 1.0)
-
-                if self.operationQueue.operationCount == 0 {
-                    self.isMonitoring = false
-                    DispatchQueue.main.async {
-                        self.onQueueEmpty?()
-                    }
-                    break
-                }
-            }
-        }
-    }
-}
- */
-
 final class DestinationGetQueue {
     static let shared = DestinationGetQueue()
     
